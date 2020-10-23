c0e15ea2f7bb9e398f806684cc2b83e31c275528
Bayardo <bjlslopez@gmail.com>
2020-10-23 00:08:08 -0600

    Terminado el consumo de Web API

--- ProyectoMVC/Controllers/HumanResources_DepartmentController.cs
+++ ProyectoMVC/Controllers/HumanResources_DepartmentController.cs
@@ -1,9 +1,12 @@
-﻿using ProyectoMVC.Models;
+﻿using Newtonsoft.Json;
+using ProyectoMVC.Models;
 using ProyectoMVC.ServiceReferenceAW;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,10 +16,20 @@ namespace ProyectoMVC.Controllers
     {
         Service1Client client = new Service1Client();
         // GET: HumanResources_Department
+        /*
         public ActionResult Index()
         {
             var select = client.SELECT_HumanResources_Departments();
             return View(select);
+        }
+        */
+        public async Task<ActionResult> Index()
+        {
+            var client = new HttpClient();
+            var json = await client.GetStringAsync("https://localhost:44309/api/Departments");
+             var result = JsonConvert.DeserializeObject<List<SELECT_HumanResources_Department_Result>>(json);
+
+            return View(result);
         }
 
         public ActionResult Insert()
--- ProyectoMVC/Controllers/Production_ProductCategoryController.cs
+++ ProyectoMVC/Controllers/Production_ProductCategoryController.cs
@@ -1,8 +1,11 @@
-﻿using ProyectoMVC.Models;
+﻿using Newtonsoft.Json;
+using ProyectoMVC.Models;
 using ProyectoMVC.ServiceReferenceAW;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,10 +15,20 @@ namespace ProyectoMVC.Controllers
     {
         Service1Client client = new Service1Client();
         // GET: Production_ProductCategory
+        /*
         public ActionResult Index()
         {
             var select = client.SELECT_Production_ProductCategory();
             return View(select);
+        }
+        */
+        public async Task<ActionResult> Index()
+        {
+            var client = new HttpClient();
+            var json = await client.GetStringAsync("https://localhost:44309/api/ProductCategories");
+            var result = JsonConvert.DeserializeObject<List<SELECT_Production_ProductCategory_Result>>(json);
+
+            return View(result);
         }
 
         public ActionResult Insert()
